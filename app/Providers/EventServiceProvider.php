6786445cc2c98f3fd3f8ccc4a989279ea17e36be
amasdeogratias <deogratiasprog@gmail.com>
2023-09-10 00:13:56 +0300

    mapped event to listeners class

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,10 +3,13 @@
 namespace App\Providers;
 
 use App\Events\AchievementUnlocked;
+use App\Listeners\HandleAchievementUnlocked;
+use App\Events\BadgeUnlocked;
+use App\Listeners\HandleBadgeUnlocked;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use App\Listeners\HandleAchievementUnlocked;
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         AchievementUnlocked::class => [
             HandleAchievementUnlocked::class,
+        ],
+
+        BadgeUnlocked::class => [
+            HandleBadgeUnlocked::class,
         ],
     ];
 
